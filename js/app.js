41aa816b4c1d1db4a07b497e0be0fc7a6a1c9e0c
joca <jscmanuelcd@gmail.com>
2026-08-27 17:43:48 +0200

    Conexión de datos en vivo de mercado en tab1.html

--- js/app.js
+++ js/app.js
@@ -131,12 +131,7 @@ function renderGraficoTV(symbol, interval) {
       "toolbar_bg": "#f1f3f6",
       "enable_publishing": false,
       "allow_symbol_change": true,
-      "container_id": "tv_chart_container",
-      // Añadir el indicador MACD automáticamente al cargar
-      "studies": [
-        "STD;MACD",
-        "STD;RSI"
-      ]
+      "container_id": "tv_chart_container"
     });
   }
 }
